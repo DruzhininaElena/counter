6a68a34f56a29c17c9552355107cba84d6fa40f3
Elena Druzhinina <elenashebrova@gmail.com>
2025-03-29 12:26:11 +0700

    added the ability to remove 0 from input

--- src/assets/components/settings/setting/Setting.tsx
+++ src/assets/components/settings/setting/Setting.tsx
@@ -3,8 +3,8 @@ import {ChangeEvent} from 'react';
 
 type Props = {
     title: string
-    value: number
-    onChangeInput: (newValue: number) => void
+    value: string
+    onChangeInput: (newValue: string) => void
     errorInputMax?: boolean
     errorInputStart?: boolean
     error: boolean
@@ -12,7 +12,10 @@ type Props = {
 export const Setting = ({ title, value, onChangeInput, error }: Props) => {
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        onChangeInput(Number(e.currentTarget.value))
+        const newValue = e.currentTarget.value;
+        if (newValue === '' || !isNaN(Number(newValue))) {
+            onChangeInput(newValue);
+        }
     }
 
     return (
